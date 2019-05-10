7bd199a8595aa4f99cff12ec2306542bcaa71379
= <=>
2019-05-10 10:34:09 +0530

    Express Added

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const dateformat = require('dateformat')
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/datetime',function(req,res){
+    res.write(dateformat(new Date(),'dd-mmm-yyyy hh:MM:ss TT'))
+    res.end()
+})
+
+app.get('/date',function(req,res){
+    res.write(dateformat(new Date(),'dd-mmm-yyyy'))
+    res.end()
+})
+
+app.get('/time',function(req,res){
+    res.write(dateformat(new Date(),'hh:MM:ss TT'))
+    res.end()
+})
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
